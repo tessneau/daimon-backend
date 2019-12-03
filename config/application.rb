e20a6799d58f602be4a41b68a1629994ff58184b
Tess Neau <tessneau@gmail.com>
2019-12-03 17:37:51 -0500

    add links to gemfile

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,20 @@ module DaimonBackend
       end
     end
 
+    #lograge for datadog
+    # Lograge config
+    # config.lograge.enabled = true
+    #
+    # # We are asking here to log in RAW (which are actually ruby hashes). The Ruby logging is going to take care of the JSON formatting.
+    # config.lograge.formatter = Lograge::Formatters::Raw.new
+    #
+    # # This is useful if you want to log query parameters
+    # config.lograge.custom_options = lambda do |event|
+    #     { :ddsource => ["ruby"],
+    #       :params => event.payload[:params].reject { |k| %w(controller action).include? k }
+    #     }
+    # end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
